7ed09f9967b818aade21d0002724cf2789549e4a
André Ramos da Silva | S2IT <andre.silva@s2it.corp>
2017-03-08 17:07:40 -0300

    updated to last spring boot version

--- src/main/java/integration/test/ContractControllerTest.java
+++ src/main/java/integration/test/ContractControllerTest.java
@@ -5,32 +5,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes=App.class)
-@WebIntegrationTest
+@WebMvcTest(controllers=ContractController.class)
 public class ContractControllerTest {
 	
 	@Autowired
-	private WebApplicationContext context;
-	
 	private MockMvc mockMvc;
-	
-	@Before
-	public void setup(){
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-	}
 	
 	@Test
 	public void test() throws Exception{
